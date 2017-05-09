f24da12096deb84186ccc1e259bbedcdf0368e16
Marco Levrero <marco.levrero@gmail.com>
2017-05-09 20:49:36 +0100

    couple of fixes

--- js/magic/eggs.js
+++ js/magic/eggs.js
@@ -4,9 +4,6 @@
 
 /*
   Easter eggs!
-  TO DO:
-  - drunk mode
-  - spaceship
 */
 
 var eggs = {
@@ -38,7 +35,7 @@ var eggs = {
     document.getElementById('waterfall').style.visibility = 'visible'
   },
 
-  // Spaceship - you can't use the arrows to control it - need to find a better solution
+  // Spaceship
   destroy: function () {
     window.location.href = "javascript:var%20KICKASSVERSION='2.0';var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='js/magic/kickass.js';void(0);"
 
